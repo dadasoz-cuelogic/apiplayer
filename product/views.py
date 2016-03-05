ff6ac72ad9be483422c3468231891d886e101284
Pranav Naxane <pranav.naxane@cuelogic.co.in>
2016-03-05 17:02:42 +0530

    Fetch all categories API

--- product/views.py
+++ product/views.py
@@ -1,3 +1,25 @@
+import json
 from django.shortcuts import render
+# from django.core.serializers import serialize
+from django.http import HttpResponse
+from product.models import Catagory
+
 
 # Create your views here.
+
+def get_all_categories(request):
+    categories = Catagory.objects.filter(is_active=True)
+    category_list = []
+    for category in categories:
+        category_list.append(
+            form_category_dictionary(category))
+
+    return HttpResponse(json.dumps(category_list),
+                        content_type='application/json')
+
+
+def form_category_dictionary(category):
+    category_dict = {}
+    category_dict['category_id'] = category.id
+    category_dict['name'] = category.name
+    return category_dict
