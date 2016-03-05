1361bc2fb404f7697c879fa29bc885e0c3840da4
Dadaso Zanzane <dadaso.zanzane@cuelogic.co.in>
2016-03-06 00:05:26 +0530

    Login and Logout functioanlity

--- apiplayer/views.py
+++ apiplayer/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponseRedirect, HttpResponse
 
 
 def index(request):
-    return render_to_response('frontend/index.html',  {})
+    return render_to_response('frontend/index.html')
