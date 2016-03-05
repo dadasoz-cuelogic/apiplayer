7d0b1e2315a7825e4f972c0618297888cdf4d2fb
Dadaso Zanzane <dadaso.zanzane@cuelogic.co.in>
2016-03-05 22:59:18 +0530

    api player js input

--- static/apiplayer/js/script.js
+++ static/apiplayer/js/script.js
@@ -6,11 +6,103 @@ $(document).ready(function(){
     });
 
     $("#method_control").click(function(){
-        console.log("test");
-        $('#method_list_holder').show()
+        $('#method_list_holder').show();
     });
 
+    $("#request_tabs li").click(function(){
+        $("#request_tabs li").removeClass('active');
+        $(this).addClass("active");
+        $("#request_params").find("table").hide();
+        table = $(this).attr("data-pk");
+        $("#request_params").find("#"+table).show();
+    });
+
+    $( "#token-model" ).dialog({
+      autoOpen: false,
+      width:400,
+      height:500,
+      modal:true,
+    });
+
+    $("#customtoken").click(function(){
+        $( "#token-model" ).dialog("open");
+    });
+ 
+
 });
+
+
+var resourse_data = null;
+
+var method_data = null;
+
+$(document).on('click', '.click_parent', function() {
+    method_id = parseInt($(this).attr('data-pk'));
+    get_method(method_id);
+    $('#method_list_holder').hide();
+    setTimeout(function(){ 
+        
+        set_method(method_data);
+
+    }, 500);
+});
+
+function set_method(data){
+    method_name = data.method.name;
+    display_name = data.method.display_name;
+    method_id = data.method.id;
+    path = data.path;
+    params = data.method.params;
+    $("#request_verb option").each(function() { this.selected = (this.text == method_name); });
+    $("#request_bar_1").val(path);
+    $("#request_tabs, #request_params").show();
+    $('.tbody_req').html("")
+    if(method_name==="GET"){
+        $("#request_params").find("table").hide();
+        $("#request_tabs li").removeClass('active');
+        $("#query_tab").addClass("active");
+        $("#request_params").find("#request_query").show();
+        $.each(params, function(index, param) {
+            required = "";
+            required_class ="";
+            required_bool ="False";
+            if(param.required){
+                required = '<sup class="sup_required">*</sup>';
+                required_class ="";
+                required_bool = "True";
+            }
+            html = '<tr class="js_tempField">';
+            html += '<td>time_sent'+param.name+required+'</td>';
+            html += '<td class="required_element" style="white-space: nowrap;"><input class="xlarge" placeholder="'+param.default+'" id="'+param.name+'" name="'+param.name+'" value="" required="'+required_bool+'" type="text"></td>';
+            html += '<td>'+param.doc+'</td>';
+            html += '</tr>';
+            $("#request_params").find("#request_query_body").append(html);
+        });
+    }
+    else{
+        $("#request_params").find("table").hide();
+        $("#request_tabs li").removeClass('active');
+        $("#body_tab").addClass("active");
+        $("#request_params").find("#request_body").show();
+        $.each(params, function(index, param) {
+            required = "";
+            required_class ="";
+            required_bool ="False";
+            if(param.required){
+                required = '<sup class="sup_required">*</sup>';
+                required_class ="";
+                required_bool = "True";
+            }
+            html = '<tr class="js_tempField">';
+            html += '<td>'+param.name+required+'</td>';
+            html += '<td class="required_element" style="white-space: nowrap;"><input class="xlarge" placeholder="'+param.default+'" id="'+param.name+'" name="'+param.name+'" value="" required="'+required_bool+'" type="text"></td>';
+            html += '<td>'+param.doc+'</td>';
+            html += '</tr>';
+            $("#request_params").find("#request_body_body").append(html);
+        });
+    }
+
+}
 
 $.postJSON = function(url, data, callback) {
     return jQuery.ajax({
@@ -32,8 +124,8 @@ function get_product_data(){
         'product_name' : product,
     }
     $.postJSON('/api/get-data/',post_data,function(data,status){
-        console.log(data);
         load_initial_data(data);
+        resourse_data = data;
     });
 }
 
@@ -41,10 +133,60 @@ function load_initial_data(data){
     $("#end_point").html("");
     endpoints = data.endpoints
     $.each(endpoints, function(index, endpoint) {
+        auth_required = 'auth_required';
         end_point_html = '<option value="'+endpoint.base+'">'+endpoint.base+'</option>';
         $("#end_point").append(end_point_html);
         $.each(endpoint.resources, function(index, section) {
-            console.log(section);
+            $.each(section, function(index, sub_section_all) {
+                api_sections = '<section class="js_activeMethod js_methodSection base_0">';                                        
+                $.each(sub_section_all, function(index, sub_section) {
+                    if(index==0){
+                        api_sections += '<strong>Create and Update Terms and Exams</strong><ul>';
+                    }else{
+                        method_name = sub_section.method.name;
+                        method_id = sub_section.method.id;
+                        display_name = sub_section.method.display_name;
+                        b_color = '';
+                        if(method_name==="GET"){
+                           b_color = 'blue'; 
+                        }else if(method_name==="POST"){
+                           b_color = 'green'; 
+                        }
+                        else if(method_name==="PUT"){
+                           b_color = 'brown'; 
+                        }
+                        else if(method_name==="DELETE"){
+                           b_color = 'red'; 
+                        }
+                        auth_required = auth_required + ' ' + b_color;
+                        api_sections += '<li class="click_parent js_methodMatch" data-pk='+method_id+' onclick="return false;" style="clear: both;"><span class="lozenge left '+auth_required+'">'+method_name+'</span><a class="provider_method click_child" title="'+display_name+'" href="javascript:void(0);">'+display_name+'</a></li>';
+                    }
+                    if(index == sub_section_all.lenght+1){
+                        api_sections += '</ul></section>';
+                    }
+                });
+                $("#resource_holder").html(api_sections);
+            });
+        });
+    });
+}
+
+
+function get_method(id){
+    endpoints = resourse_data.endpoints
+    $.each(endpoints, function(index, endpoint) {
+        $.each(endpoint.resources, function(index, section) {
+            $.each(section, function(index, sub_section_all) {
+                $.each(sub_section_all, function(index, sub_section) {
+                    if(index != 0){
+                        method_id = sub_section.method.id;
+                        if(method_id === id){
+                            method_data = sub_section;
+                            return;
+                        }
+                    }
+                });
+            });
         });
     });
 }
