6aa1047761f49ea7a4bb66652941dc10a4a24d2b
Pranav Naxane <pranav.naxane@cuelogic.co.in>
2016-03-05 14:20:58 +0530

    Added add_api.html

--- organization/urls.py
+++ organization/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from organization import views
 
 urlpatterns = [
-    url(r'^', views.dashboard, name="dashboard"),
+    url(r'^$', views.dashboard, name="dashboard"),
+    url(r'^add-api/$', views.add_api, name="add_api"),
 ]
